2b9383d7bf71e0f8fd4e6d97caf1227ff08a0fa1
Eric Adams <ericjamesadams@gmail.com>
2012-10-31 13:17:33 -0600

    added app key

--- assets/facebook-php-sdk/examples/with_js_sdk.php
+++ assets/facebook-php-sdk/examples/with_js_sdk.php
@@ -3,8 +3,8 @@
 require '../src/facebook.php';
 
 $facebook = new Facebook(array(
-  'appId'  => '344617158898614',
-  'secret' => '6dc8ac871858b34798bc2488200e503d',
+  'appId'  => '274129836023804',
+  'secret' => 'ada2fa9bfa82ddee3da31d501b9dba86',
 ));
 
 // See if there is a user from a cookie
@@ -24,10 +24,11 @@ if ($user) {
 <!DOCTYPE html>
 <html xmlns:fb="http://www.facebook.com/2008/fbml">
   <body>
-    <?php if ($user) { ?>
-      Your user profile is
+    <?php if ($user) {
+    	ob_start(); 
+      header('location:welcome.php');?>
       <pre>
-	<?php print htmlspecialchars(print_r($user_profile, true)) ?>
+	<?php //print htmlspecialchars(print_r($user_profile, true)) ?>
       </pre>
     <?php } else { ?>
       <fb:login-button></fb:login-button>
